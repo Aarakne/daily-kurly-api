1ef6d554b747513d5648238e1ca25236d6ff6115
mochang2 <qjsrodksro@gmail.com>
2022-08-21 13:35:26 +0900

    본인이 좋아요 누른 게시글 보기 api 구현

--- src/route/me/index.ts
+++ src/route/me/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import { ifNotLoggedIn, verifyToken } from '../../lib/auth.helper'
-import { getMyPosts } from './me'
+import { getMyPosts, getLikedPosts } from './me'
 
 const router = express.Router()
 
-router.get('/posts', ifNotLoggedIn, verifyToken, getMyPosts)
+router
+  .get('/posts', ifNotLoggedIn, verifyToken, getMyPosts)
+  .get('/likedPosts', ifNotLoggedIn, verifyToken, getLikedPosts)
 
 export default router
--- src/route/me/me.ts
+++ src/route/me/me.ts
@@ -27,3 +27,28 @@ export const getMyPosts = async (req: Request, res: Response) => {
     return response(res, 500)
   }
 }
+
+export const getLikedPosts = async (req: Request, res: Response) => {
+  try {
+    const username = getUserName(req)
+
+    const user = await User.findOne({ username })
+      .populate({
+        path: 'likedPosts',
+        select: '_id content',
+        match: { deleted: false },
+        options: { sort: { createdAt: -1 } },
+      })
+      .select('likedPosts')
+
+    if (!user) {
+      console.error('존재하지 않는 유저입니다.')
+      return response(res, 401)
+    }
+
+    return response(res, 200, { likedPosts: user.likedPosts })
+  } catch (err) {
+    console.error(err)
+    return response(res, 500)
+  }
+}
