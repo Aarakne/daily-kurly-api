f0c0382166cb48f3c18f0e408cbc0692c0d37d0f
mochang2 <qjsrodksro@gmail.com>
2022-08-24 00:15:47 +0900

    로그인 시 username 전송 추가

--- src/route/auth/auth.ts
+++ src/route/auth/auth.ts
@@ -68,7 +68,7 @@ export const verifyUser = async (req: Request, res: Response) => {
       // sameSite: 'strict',
     })
 
-    return response(res, 200, { grade: user[0].grade })
+    return response(res, 200, { grade: user[0].grade, username })
   } catch (err) {
     console.error(err)
     return response(res, 500)
